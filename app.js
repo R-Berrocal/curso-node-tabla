202d7b9105362587f133c9fb42dd8b52658b8ffa
Roberto Berrocal <robertoberro20@gmail.com>
2021-12-06 21:37:03 -0500

    Repasando el curso de Node

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 
-const {crearArchivo}=require("./helpers/multiplicar");
-const argv= require("./config/yargs");
+const argv=require("./config/yargs")
+const {crearArchivo}= require("./helpers/multiplicar");
 require("colors");
 
-console.clear();
-console.log(process.argv);
 
 
-crearArchivo(argv.b, argv.l,argv.h)
-.then(nombreArchivo=>console.log(nombreArchivo.rainbow,"creado"))
-.catch(err=>console.log(err));
+// const [,,arg3="base=3"]=process.argv;
+// const [,base=5]=arg3.split("=");
+
+crearArchivo(argv.b,argv.l,argv.h).then(nombreArchivo=>console.log(nombreArchivo.green,"creado".green)).catch(err=>console.log(err));
 
 
--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,37 +1,29 @@
-const fs= require("fs");
-const colors= require("colors");
-const crearArchivo=async(base=5, listar=false,hasta=10)=>{
-    try{
+const fs=require("fs")
+const color=require("colors");
+const crearArchivo=async(base=5,listar=false,hasta=10)=>{
+    try {
+        
         let salida="";
-        let consola=""
-        for(let i=1; i<=hasta;i++){
+        let consola="";
+        for(let i=1;i<=hasta;i++){
             salida+=`${base} x ${i} = ${base*i}\n`;
-            consola+=`${base} ${"x".rainbow} ${i}${"=".rainbow} ${base*i}\n`;
-
+            consola+=`${base} ${"x".red} ${i} ${"=".red} ${base*i}\n`;
         }
         if(listar){
-            console.log("=============== \n tabla del:".rainbow
-            ,base,"\n================".rainbow);
+            console.log("==============".red);
+            console.log(`Tabla del ${base}`.red);
+            console.log("==============".red);
             console.log(consola);
-        } 
-        
-        /*
-        fs.writeFile(`tabla-${base}.txt`,salida,(err)=>{
-            if(err) throw err;
-            console.log(`tabla-${base}.txt creado`);
-        });
-        */
-        
-        fs.writeFileSync(`./salida/tabla-${base}.txt`,salida);
-        
+        }
     
-        return `tabla-${base}.txt `;
-    
-    }catch(err){
-        console.log(err);
+        fs.writeFileSync(`./Salida/tabla del ${base}.txt`,salida);
+        return `tabla del ${base}`;
+        
+    } catch (err) {
+        throw err
     }
-    }
+}
 
 module.exports={
-    crearArchivo: crearArchivo
+    crearArchivo
 }
